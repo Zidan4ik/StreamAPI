a5dda837027ad599b30aa4dcdbb22d8085b2809d
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-03-09 22:35:18 +0200

    add peek method

--- src/examples/stream/StreamIntermediate.java
+++ src/examples/stream/StreamIntermediate.java
@@ -1,6 +1,9 @@
 package examples.stream;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -18,13 +21,21 @@ public class StreamIntermediate {
         Stream<Integer> sort = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5)
                 .sorted((x, y) -> x - y);
 
-        Stream<Integer> map = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5).map(value -> value + 10);
+        Stream<Integer> map = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5)
+                .map(value -> value + 10);
 
-        Stream<Integer> distinct = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5).distinct();
+        Stream<Integer> distinct = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5)
+                .distinct();
 
-        Stream<Integer> skip = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5).skip(5);
+        Stream<Integer> skip = Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5)
+                .skip(5);
 
-        IntStream mapToInt = Stream.of("1", "6", "3", "1", "8", "92", "51", "12", "0", "-5").mapToInt(value -> Integer.parseInt(value));
+        IntStream mapToInt = Stream.of("1", "6", "3", "1", "8", "92", "51", "12", "0", "-5")
+                .mapToInt(value -> Integer.parseInt(value));
+        Set<Integer> peek = Stream.of(1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+                .filter(v -> v >= 3)
+                .peek(v -> System.out.print(v + " "))
+                .collect(Collectors.toSet());
 
 //        System.out.println("Intermediate Operator");
 //        System.out.println("Main Collection for research: " + Stream.of(1, 6, 3, 1, 8, 92, 51, 12, 0, -5).collect(Collectors.toList()));
@@ -35,12 +46,10 @@ public class StreamIntermediate {
 //        System.out.println("Skip: " + skip.collect(Collectors.toList()));
 //        System.out.println("MapToInt: " + Arrays.toString(mapToInt.toArray()));
 
-//        List<Integer> collect = filter2.filter(x -> x >= 5).collect(Collectors.toList());
-//        filter2.collect(Collectors.toList());
 
-        // no use more than once instances type of Stream, as instance
+//      no use more than once instances type of Stream, as instance
 //        Stream<Integer> stream = filter2.filter(x -> x >= 5);
-//        List<Integer> collectFilter2 = stream.collect(Collectors.toList());
+//        List<Integer> collectFilter2 = filter2.collect(Collectors.toList());
 //        System.out.println(collectFilter2);
 
     }
